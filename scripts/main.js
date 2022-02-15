91478652aad67c6b83045e98ead45945903625dc
anjon mahamood <anjon.cu.cse@gmail.com>
2022-02-15 23:43:45 +0600

    Added animated svg on scroll

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,6 @@
 const header = document.querySelector('header');
+const blobs = document.querySelectorAll('g.blob');
+const sections = document.querySelectorAll('section');
 
 function easeIn(x) {
   return x * x * x;
@@ -9,6 +11,20 @@ function fadeHeader() {
   header.style.opacity = 1 - easeIn(scrolledDistance / 500);
 }
 
+function animateBlobs() {
+  const scrolledDistance = window.pageYOffset;
+
+  blobs.forEach((blob, index) => {
+    const currentSection = sections[index];
+    if (scrolledDistance > currentSection.offsetTop - 300) {
+      blob.classList.add('in-view-path');
+    } else {
+      blob.classList.remove('in-view-path');
+    }
+  });
+}
+
 window.addEventListener('scroll', () => {
   fadeHeader();
+  animateBlobs();
 });
